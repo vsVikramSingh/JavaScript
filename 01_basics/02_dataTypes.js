387bd3fb859f11b4a7e071866ed07a987b23096f
vsVikramSingh <vsvikramsingh5200@gmail.com>
2023-11-11 04:12:00 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"  // treat all JS code as newer version
+
+
+let name = "vikram"
+let age = 27
+let isLoggedIn = false
+
+
+// number
+// string
+// boolean
+// bigint
+// null => standalone value
+// undefined => unsigned value
+// sumbol => unique
+// 
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)  // object
