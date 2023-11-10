5d30c26e99b340392c7258e1a0fa990001319f25
vsVikramSingh <vsvikramsingh5200@gmail.com>
2023-11-11 03:53:05 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1234
+let accountEmail ="vikram@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+/*
+Prefer not to use var
+bcs of issue in block scope and functional scope 
+*/
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity]);
