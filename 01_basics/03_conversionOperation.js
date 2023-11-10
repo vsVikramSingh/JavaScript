ae23f0f0322105b06f91ccc29dcba1922ad8ee3b
vsVikramSingh <vsvikramsingh5200@gmail.com>
2023-11-11 04:53:09 +0530

    conversions and operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+// "33" => 33
+// "33abc" => NaN
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); // true
+
+// 1 => true; 0 => false
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber);  // 33
+console.log(typeof stringNumber); // stirng
+
+
+
+//********************* Operations ********************
+
+let value =3
+let negValue = -value
+console.log(negValue) // -3
+
+console.log(2+2) // 4
+console.log(2-2) // 0
+console.log(2*2) // 4
+console.log(2/2) // 1
+console.log(2%2) // 0
+
+
+let str1 = "hello"
+let str2 = "Vikram"
+let str3 = str1+str2
+console.log(str3) // helloVikram
+
+
+
+console.log("1" + 2) // 12
+console.log(2 + "2") // 22
+console.log("2" + 2 + 2) // 222
+console.log(2 + 2 + "2") // 42
+
+
+
+console.log((3 + 4) * 5 % 3) //2 must use seperate () 
+
+ console.log(+true) // 1
+ console.log(+"") // 0
+
+ 
