5f04e66a649c0da119ca1fc2db290454b61f6984
vsVikramSingh <vsvikramsingh5200@gmail.com>
2023-11-11 05:37:41 +0530

    changes in data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,15 @@
 "use strict"  // treat all JS code as newer version
 
 
+
+// Primitive  7 types
+//            String , Number , Boolean , null , undefined , Symbol
+
+// Reference (Non primitive)
+//            Array , Objects , Functions
+
+
+
 let name = "vikram"
 let age = 27
 let isLoggedIn = false
@@ -17,3 +26,26 @@ let isLoggedIn = false
 
 console.log(typeof undefined) // undefined
 console.log(typeof null)  // object
+
+
+const id = Symbol('1')
+const otherId = Symbol('1') 
+console.log(id) // Symbol(1)
+console.log(otherId) // Symbol(1)
+console.log(id === otherId) // false
+
+
+const heros = ["Vikram", "Harsha" , "Mitya", "Shubha"];
+
+
+let myObj = {
+    name: "Vikram",
+    age: 27
+}
+
+
+const myFunction = function(){
+    console.log("Hello vikram")
+}
+
+ 
